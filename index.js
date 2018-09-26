0064dffa6033207842450f7befc4a1f36b289698
lixinliang <393464140@qq.com>
2018-09-26 09:50:04 +0800

    add

---
+++ index.js
@@ -0,0 +1,17 @@
+'use es6'
+
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+  console.log('')
+  console.log(req['url'])
+  console.log(req['headers']['user-agent'])
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
+  res.end('hello world')
+})
+
+app.listen(8000)
+
+console.log('server is running on "http://localhost:8000/"')
